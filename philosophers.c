556887b3bc59b4645404c46f3eb37bef3dbac4bb
jgirard- <jgirard-@c2r9p2.42nice.fr>
2022-11-23 16:36:56 +0100

    Error msg fixed

--- philosophers.c
+++ philosophers.c
@@ -6,7 +6,7 @@
 /*   By: jgirard- <jgirard-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 15:36:46 by jgirard-          #+#    #+#             */
-/*   Updated: 2022/11/23 16:22:24 by jgirard-         ###   ########.fr       */
+/*   Updated: 2022/11/23 16:35:59 by jgirard-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ void	initphvars(int ac, char **av, t_struct *s)
 {
 	if (ac < 5 || ac > 6)
 	{
-		error("\033[32;1mUsage:\033[0m./philosophers "
-			"\033[92;1mN_OF_PHILO T_TO_EAT "
-			"T_TO_DIE T_TO_SLEEP \033[93;1mN_OF_T_PHILO_EAT");
+		error("\033[91;1mError: Bad Params\n""\033[95;1mUsage:\033[0m./philosophers "
+			"\033[92;1mNUM_OF_PHILO TIME_TO_EAT "
+			"TIME_TO_DIE TIME_TO_SLEEP \033[93;1mNUM_OF_TIMES_PHILOS_EAT");
 	}
 	s->phil.nphilo = ft_atoi(av[1]);
 	s->time.tto_die = ft_atoi(av[2]);
@@ -34,11 +34,11 @@ void	initphvars(int ac, char **av, t_struct *s)
 	s->time.num_eat = ft_atoi(av[5]);
 	else
 		s->time.num_eat = 0;
-	/*printf("Number of philosophers: %d\n", s->phil.nphilo);
+	printf("Number of philosophers: %d\n", s->phil.nphilo);
 	printf("Time to die: %d\n", s->time.tto_die);
 	printf("Time to eat: %d\n", s->time.tto_eat);
 	printf("Time to sleep: %d\n", s->time.tto_sleep);
-	printf("Number of times each philosopher must eat: %d\n", s->time.num_eat);*/
+	printf("Number of times each philosopher must eat: %d\n", s->time.num_eat);
 }	
 
 int	main(int ac, char **av)
@@ -46,5 +46,6 @@ int	main(int ac, char **av)
 	t_struct	s;
 
 	initphvars(ac, av, &s);
+	
 	return (0);
 }
