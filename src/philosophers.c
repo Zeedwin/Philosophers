3414590690d6055ca10def5ff80dba3be609b383
jgirard- <jgirard-@c2r1p6.42nice.fr>
2022-12-01 15:59:20 +0100

    o calme on sleep

--- src/philosophers.c
+++ src/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: jgirard- <jgirard-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 15:36:46 by jgirard-          #+#    #+#             */
-/*   Updated: 2022/11/27 17:21:45 by jgirard-         ###   ########.fr       */
+/*   Updated: 2022/11/27 18:13:27 by jgirard-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,21 @@ void	error(char *c)
 	exit(1);
 }
 
+void	badargs()
+{
+	error("\033[91;1mError: Bad Params\n""\033[95;1mUsage:\033[0m./philosophers "
+	"\033[92;1mNUMBER_OF_PHILOS TIME_TO_EAT "
+	"TIME_TO_DIE TIME_TO_SLEEP \033[93;1mNUMBER_OF_TIMES_PHILOS_EAT");
+}
+
 void	initphvars(int ac, char **av, t_struct *s)
 {
 	if (ft_atoi(av[1]) >= 200)
 	{
 		error("\033[91;1m Exeeded maximum number of philosophers");
 	}
-	if (ac < 5 || ac > 6)
-	{
-		error("\033[91;1mError: Bad Params\n""\033[95;1mUsage:\033[0m./philosophers "
-			"\033[92;1mNUMBER_OF_PHILOS TIME_TO_EAT "
-			"TIME_TO_DIE TIME_TO_SLEEP \033[93;1mNUMBER_OF_TIMES_PHILOS_EAT");
-	}
-	else
-		s->phil.philo = malloc(sizeof(ft_atoi(av[1])));
-		s->phil.fork = malloc(sizeof(ft_atoi(av[1])));
+	s->phil.philo = malloc(sizeof(ft_atoi(av[1])));
+	s->phil.fork = malloc(sizeof(ft_atoi(av[1])));
 	s->phil.nphilo = ft_atoi(av[1]);
 	s->time.tto_die = ft_atoi(av[2]);
 	s->time.tto_eat = ft_atoi(av[3]);
@@ -47,7 +47,7 @@ void	initphvars(int ac, char **av, t_struct *s)
 	printf("Time to eat: %d\n", s->time.tto_eat);
 	printf("Time to sleep: %d\n", s->time.tto_sleep);
 	printf("Number of times each philosopher must eat: %d\n", s->time.num_eat);
-}	
+}
 
 void	*philosopher(void *arg)
 {
@@ -73,15 +73,19 @@ int	main(int ac, char **av)
 {
 	t_struct	s;
 	//int			*res;
-	//printf("entering main\n");
+	if (ac < 5 || ac > 6)
+	{
+		badargs();
+	}
 	initphvars(ac, av, &s);
+	printf("entering main\n");
 	for (int i = 0; i < 10; i++)
 	{
 		if (pthread_create(s.phil.philo, NULL, philosopher, &s) != 0)
 			return	(1);
 	}
 	
-	//printf("thread created\n");
+	printf("thread created\n");
 	//philosopher(s);
 	if (pthread_join(*(s.phil.philo), NULL) != 0)
 		return (2);
